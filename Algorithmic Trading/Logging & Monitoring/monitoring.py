5e9d047bfb4d77a418261de0d192b24053782c99
Adalet Adiljan <130295885+Adaleet@users.noreply.github.com>
2024-10-29 23:14:10 +0100

    Update monitoring.py

--- Algorithmic Trading/Logging & Monitoring/monitoring.py
+++ Algorithmic Trading/Logging & Monitoring/monitoring.py
@@ -1,1 +1,16 @@
-text for test
+import psutil
+import time
+
+def monitor_cpu():
+    cpu_usage = psutil.cpu_percent(interval=1)
+    print(f"CPU Usage: {cpu_usage}%")
+
+def monitor_memory():
+    memory = psutil.virtual_memory()
+    print(f"Memory Usage: {memory.percent}%")
+
+'''if __name__ == "__main__":
+    while True:
+        monitor_cpu()
+        monitor_memory()
+        time.sleep(5)'''
